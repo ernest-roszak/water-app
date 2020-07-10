8b5818bad9b951014f84a9822f4666adde0bcb1c
Ernest Roszak <ernest.roszak89@gmail.com>
2020-07-10 20:12:11 +0200

    js add minus buttons implement

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,39 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
-import {registerSW} from './pwa.js';
+import { registerSW } from "./pwa.js";
 registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const glass = document.querySelector(".glass--js");
+const add = document.querySelector(".add--js");
+const minus = document.querySelector(".minus--js");
+const info = document.querySelector(".info--js");
+
+let sum = 0;
+
+glass.innerHTML = `${sum}`;
+
+add.addEventListener("click", () => {
+  sum = sum + 1;
+  glass.innerHTML = `${sum}`;
+  console.log(sum);
+  if (sum != 0) {
+    info.innerHTML = ``;
+  }
+});
+
+minus.addEventListener("click", () => {
+  if (sum > 0) {
+    sum = sum - 1;
+    glass.innerHTML = `${sum}`;
+    console.log(sum);
+  } else {
+    info.innerHTML = `ℹ️ You don't have any glass yet. First, add a glass of water`;
+  }
+});
+
+for (sum = 0; i < 8; i++) {
+  console.log("cel");
+}
