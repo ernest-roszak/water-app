220c0ce6826c6818b29154a4a580aca21504e0ea
Ernest Roszak <ernest.roszak89@gmail.com>
2020-07-13 15:32:55 +0200

    date implement

--- src/js/index.js
+++ src/js/index.js
@@ -10,51 +10,43 @@ const glass = document.querySelector(".glass--js");
 const add = document.querySelector(".add--js");
 const minus = document.querySelector(".minus--js");
 const info = document.querySelector(".info--js");
-const key = new Date().toISOString().slice(0, 10)
+const key = new Date().toLocaleString().slice(0, 10);
 
 let sum = 0;
-
-if (sum) {
-    sum = localStorage.setItem(0);
-} else {
-
-};
+const keyValue = localStorage.getItem(key);
 
 glass.innerHTML = `${sum}`;
 
-if (key) {
-    localStorage.setItem("key", 0);
+if (keyValue) {
+  sum = keyValue;
 } else {
-    
-};
+  localStorage.setItem(key, 0);
+}
 
 add.addEventListener("click", () => {
-    localStorage.setItem("key", sum +1);
-  sum = sum + 1;
+  sum++;
   glass.innerHTML = `${sum}`;
-  console.log(sum);
+  localStorage.setItem(key, sum);
   if (sum != 0) {
     info.innerHTML = ``;
   }
   if (sum > 9) {
-    glass.classList.add('glass__content--duble');
-    }
+    glass.classList.add("glass__content--duble");
+  }
 });
 
-
 minus.addEventListener("click", () => {
-    localStorage.setItem("key", sum - 1);
+  
   if (sum > 0) {
-    sum = sum - 1;
+    sum = parseInt(sum) - 1;
     glass.innerHTML = `${sum}`;
-    console.log(sum);
-  } else {
+    } else {
     info.innerHTML = `ℹ️ You don't have any glass yet. First, add a glass of water`;
   }
   if (sum < 10) {
-    glass.classList.remove('glass__content--duble');
-    }
+    glass.classList.remove("glass__content--duble");
+  }
+  localStorage.setItem(key, sum);
 });
-
 
 console.log(`to twoje ${key}`);
